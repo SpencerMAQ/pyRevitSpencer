d72f86e361bec05ab078a651ae86b86fa0710fb3
Spencer <spencer.michael.q@gmail.com>
2018-01-02 20:05:53 +0800

    added basic working add-on, Hello world

---
+++ spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/spencer_script.py
@@ -0,0 +1,9 @@
+"""Basics of transactions. Updates comment parameter on all sheets and delete all walls."""
+
+__title__ = 'First\nTransactions'
+__author__ = 'Ehsan Iran-Nejad'
+
+
+from Autodesk.Revit.UI import TaskDialog
+
+TaskDialog.Show('spencerCorePyRevut', 'Hello World!')
