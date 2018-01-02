71f3d90cf78240e928e9d64f34fa93ce79408ec9
Spencer <spencer.michael.q@gmail.com>
2018-01-02 20:20:43 +0800

    added basic working add-on, Hello world

--- spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/spencer_script.py
+++ spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/spencer_script.py
@@ -1,7 +1,12 @@
-"""Basics of transactions. Updates comment parameter on all sheets and delete all walls."""
+"""Just a script that
+Displays Hello world."""
 
 __title__ = 'Hello\nWorld'
-__author__ = 'Michael Spence Quinto'
+__author__ = 'Michael Spencer Quinto'
+
+# __context__ = 'Selection'
+# Tools are active even when there are no documents available/open in Revit
+__context__ = 'zerodoc'
 
 
 from Autodesk.Revit.UI import TaskDialog
