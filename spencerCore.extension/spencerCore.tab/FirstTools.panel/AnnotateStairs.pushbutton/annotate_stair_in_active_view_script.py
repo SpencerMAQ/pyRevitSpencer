aadbf86dbf7e5eea143161b32c13ffda19a072f4
Spencer <spencer.michael.q@gmail.com>
2018-01-02 22:54:12 +0800

    the hell is the problem with OwnerView related methods? That's it for today

--- spencerCore.extension/spencerCore.tab/FirstTools.panel/AnnotateStairs.pushbutton/annotate_stair_in_active_view_script.py
+++ spencerCore.extension/spencerCore.tab/FirstTools.panel/AnnotateStairs.pushbutton/annotate_stair_in_active_view_script.py
@@ -11,32 +11,49 @@ import clr
 
 from revitutils import doc, uidoc
 
-from Autodesk.Revit.DB import Transaction, FilteredElementCollector, LinkElementId, View
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector, LinkElementId, View, ViewPlan, ViewType
 from Autodesk.Revit.DB.Architecture import StairsPath, Stairs, StairsPathType
 
 
-stair_collector         = FilteredElementCollector(doc)                 \
-                            .OfClass(clr.GetClrType(Stairs))            \
-                            .ToElements()
+
 
 default_path_type_id    = FilteredElementCollector(doc)                 \
                             .OfClass(clr.GetClrType(StairsPathType))    \
                             .ToElementIds()[0]
                             # .FirstElementId()
 
+# TODO: filter only to plan views using FilteredElementCollector
 view_collector          = FilteredElementCollector(doc)                 \
-                            .OfClass(clr.GetClrType(View))              \
-                            .ToElementIds()
+                            .OfClass(clr.GetClrType(ViewPlan))          \
+                            .ToElements()
 
 if __name__ == '__main__':
     with Transaction(doc, 'pyRevit Annotate StairPath for All Active') as t:
         t.Start()
 
-        for stair in stair_collector:
-            StairsPath.Create(doc,
-                              LinkElementId(stair.Id),
-                              default_path_type_id,
-                              doc.ActiveView.Id
-                              )
+        for plan_view in view_collector:
+            # Note: .OwnedByView(plan_view.Id) gives null results
+            # THE HELL is there a problem with OWNERVIEWID???
+            stair_collector = FilteredElementCollector(doc)             \
+                                .OfClass(clr.GetClrType(Stairs)) \
+                                .ToElements()
+            # print(stair_collector)
+            # if stair_collector:
+            #     print('hoola')
+
+
+            # if plan_view.ViewType == ViewType.FloorPlan:
+            for stair in stair_collector:
+
+                # apparently stair.OwnerViewId returns an invalid elemId
+                print(stair.Id.ToString(), stair.OwnerViewId.ToString(), plan_view.Id.ToString())
+                # test
+                if stair.OwnerViewId == plan_view.Id:
+
+                    StairsPath.Create(doc,
+                                      LinkElementId(stair.Id),
+                                      default_path_type_id,
+                                      plan_view.Id
+                                      )
 
         t.Commit()
