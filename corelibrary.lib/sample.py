cfca5054143a63b2700a5f0b90df7a571fa3a2c6
Spencer <spencer.michael.q@gmail.com>
2018-01-02 20:31:58 +0800

    added core libraries, verified it works inside pushbutton

---
+++ corelibrary.lib/sample.py
@@ -0,0 +1,12 @@
+"""core library sample"""
+
+__title__ = 'Hello\nWorld'
+__author__ = 'Michael Spencer Quinto'
+
+# __context__ = 'Selection'
+# Tools are active even when there are no documents available/open in Revit
+# __context__ = 'zerodoc'
+
+
+def sample_func():
+    return 'Hello from corelibrary.lib {}'.format(__name__)
--- spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/spencer_script.py
+++ spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/spencer_script.py
@@ -6,9 +6,13 @@ __author__ = 'Michael Spencer Quinto'
 
 # __context__ = 'Selection'
 # Tools are active even when there are no documents available/open in Revit
-__context__ = 'zerodoc'
+# __context__ = 'zerodoc'
 
 
 from Autodesk.Revit.UI import TaskDialog
 
 TaskDialog.Show('spencerCorePyRevut', 'Hello World!')
+
+if __name__ == '__main__':
+    from sample import sample_func
+    print(sample_func())
