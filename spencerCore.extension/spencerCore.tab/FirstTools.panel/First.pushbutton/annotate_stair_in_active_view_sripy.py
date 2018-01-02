383eb4dd00a6544bd5962057916e1f2cf183a068
Spencer <spencer.michael.q@gmail.com>
2018-01-02 21:30:55 +0800

    not yet tested

--- spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/annotate_stair_in_active_view_sripy.py
+++ spencerCore.extension/spencerCore.tab/FirstTools.panel/First.pushbutton/annotate_stair_in_active_view_sripy.py
@@ -7,9 +7,34 @@ __title__ = 'StairPath\nAllView'
 __author__ = 'Michael Spencer Quinto'
 
 
+import clr
+
 from revitutils import doc, uidoc
 
-from Autodesk.Revit.DB import Transaction
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector
+from Autodesk.Revit.DB.Architecture import StairsPath
 
+
+stair_collector = FilteredElementCollector(doc)                 \
+                        .OfClass(clr.GetClrType(Stairs))        \
+                        .ToElements()
+
+default_path_type_id = FilteredElementCollector(doc)            \
+                        .OfClass(clr.GetClrType(StairsPath))    \
+                        .ToElementIds()                         \
+                        .FirstElementId()
+
+# default_path_type_id = doc.GetDefaultElementTypeId()
 
 if __name__ == '__main__':
+    with Transaction(doc, 'pyRevit Annotate StairPath for All Active') as t:
+        t.Start()
+
+        for stair in stair_collector:
+            StairsPath.Create(doc,
+                              stair.Id,
+                              default_path_type_id,
+                              doc.ActiveView.Id
+                              )
+
+        t.Commit()
